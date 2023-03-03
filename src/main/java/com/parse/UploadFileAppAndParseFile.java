e4dbaaa1c598c49d6e1ca8a4db980c685c650a4d
paweljawa <pawelbojaryn@gmail.com>
2023-03-03 18:46:53 +0100

    version 1.1 review

--- src/main/java/com/parse/UploadFileAppAndParseFile.java
+++ src/main/java/com/parse/UploadFileAppAndParseFile.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UploadFileAppAndParseFile {
+class UploadFileAppAndParseFile {
 
     public static void main(String[] args) {
         SpringApplication.run(UploadFileAppAndParseFile.class, args);
